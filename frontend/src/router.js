01f9293fdad09bd0f2b9aa835df7f4a623553f95
FloSoftwareDev <floriangrootkb@gmail.com>
2025-03-24 13:59:25 +0100

    alles

---
+++ frontend/src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './components/login.vue';
+import Dashboard from './components/dashboard.vue';
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/dashboard', component: Dashboard }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
